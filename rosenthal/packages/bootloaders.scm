fb9b069154b9be2fd18062d64e184b411fcab411
Hilton Chain <hako@ultrarare.space>
2024-07-12 16:02:11 +0800

    rosenthal: grub-efi-luks2: Update patches for GRUB 2.12.
    * rosenthal/packages/bootloaders.scm (grub-luks2-argon2-support-patch): Update
    for GRUB 2.12.
    (grub-runtime-memregion-alloc-patch,grub-install-luks2-fix-patch): Delete
    variables.
    (grub-efi-luks2)[source]<patches>: Remove them.

--- rosenthal/packages/bootloaders.scm
+++ rosenthal/packages/bootloaders.scm
@@ -14,35 +14,15 @@
 ;; Patches obtained from:
 ;; <https://leo3418.github.io/collections/gentoo-config-luks2-grub-systemd/packages.html>
 
-(define grub-runtime-memregion-alloc-patch
-  (origin
-    (method url-fetch)
-    (uri (string-append "https://leo3418.github.io/"
-                        "res/collections/gentoo-config-luks2-grub-systemd/"
-                        "4500-grub-2.06-runtime-memregion-alloc.patch"))
-    (sha256
-     (base32
-      "1ynlyaabdlw7g4pqr4sc7jdqn9riwms637cqhlqnyi8jp6cs4a6x"))))
-
 (define grub-luks2-argon2-support-patch
   (origin
     (method url-fetch)
     (uri (string-append "https://leo3418.github.io/"
                         "res/collections/gentoo-config-luks2-grub-systemd/"
-                        "5000-grub-2.06-luks2-argon2-v4.patch"))
+                        "grub-2.12-luks2-argon2-v4.patch"))
     (sha256
      (base32
-      "0jfyl2j9bh0225iqq36gy55p2l96zhhhsk5r53aqmlivvxpjlf7g"))))
-
-(define grub-install-luks2-fix-patch
-  (origin
-    (method url-fetch)
-    (uri (string-append "https://leo3418.github.io/"
-                        "res/collections/gentoo-config-luks2-grub-systemd/"
-                        "9500-grub-AUR-improved-luks2.patch"))
-    (sha256
-     (base32
-      "0hn0nkp5cfsyvvyfj3vbw0z4c2swdi6gvxradl401lqvwsvq60p4"))))
+      "02y15k6rd5vj2shfijyhq2nr2775vpa55ijfy6bb8irpnh8i2272"))))
 
 (define-public grub-efi-luks2
   (let ((base grub-efi))
@@ -55,9 +35,7 @@
            (inherit base)
            (patches
             (append (origin-patches base)
-                    (list grub-runtime-memregion-alloc-patch
-                          grub-luks2-argon2-support-patch
-                          grub-install-luks2-fix-patch))))))
+                    (list grub-luks2-argon2-support-patch))))))
       (arguments
        (substitute-keyword-arguments (package-arguments base)
          ((#:configure-flags flags ''())
