97b9e2436a40f64c36a43c5d12c484d1a2afda61
Hilton Chain <hako@ultrarare.space>
2024-10-31 14:19:57 +0800

    rosenthal: Remove libinput-minimal-1.24.0. [upstreamed]
    * rosenthal/packages/freedesktop.scm (libinput-minimal-1.24.0): Delete variable.

--- rosenthal/packages/freedesktop.scm
+++ rosenthal/packages/freedesktop.scm
@@ -8,23 +8,3 @@
   #:use-module (gnu packages check)
   #:use-module (gnu packages freedesktop)
   #:use-module (gnu packages python))
-
-;; https://issues.guix.gnu.org/65525
-;; Now in core-updates.
-(define-public libinput-minimal-1.24.0
-  (package
-    (inherit libinput-minimal)
-    (name "libinput-minimal")
-    (version "1.24.0")
-    (source (origin
-              (method git-fetch)
-              (uri (git-reference
-                    (url "https://gitlab.freedesktop.org/libinput/libinput.git")
-                    (commit version)))
-              (file-name (git-file-name name version))
-              (sha256
-               (base32
-                "0xk0dljykjfmkks7kjxvbia6g3wadmy7lihfygm8icywkq8j0dw1"))))
-    (native-inputs
-     (modify-inputs (package-native-inputs libinput)
-       (append python-minimal-wrapper python-pytest)))))
